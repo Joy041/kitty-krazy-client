aa5230e6a3c4e98bccac13c3f002ad5071538e59
Joy Purkistha <joypurokisthosh@gmail.com>
2023-05-21 00:54:49 +0600

    make all toys in tabular form

--- src/MapPage/ShowToys/ShowToys.jsx
+++ src/MapPage/ShowToys/ShowToys.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom";
 
 
+
 const ShowToys = ({ toy }) => {
 
-    const { _id, name, seller, photo, price, ratting, quantity, category} = toy
+    const { _id, name, seller, photo, price, quantity, category } = toy
 
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={photo} className="h-72" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title text-3xl font-bold">{name}</h2>
-                <p><span className="font-bold">Price :</span> <span className="text-orange-400 ">{price} tk</span></p>
-                <p><span className="font-bold">Quantity :</span> {quantity}</p>
-                <p><span className="font-bold">Category :</span> {category}</p>
-                <p><span className="font-bold">Seller :</span> {seller}</p>
-                <p><span className="font-bold">Ratting :</span> {ratting}</p>
-                <div className="card-actions justify-end">
-                    <Link to={`/details/${_id}`}> <button className="btn text-base bg-pink-500 rancho border-0">View Details</button> </Link>
+        <tr>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle w-32 h-32">
+                            <img src={photo} alt="Avatar Tailwind CSS Component" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </td>
+            <td className="font-bold text-xl">{name}</td>
+            <td className="text-orange-400">{price} tk</td>
+            <td>{quantity}</td>
+            <td>{category}</td>
+            <td>{seller}</td>
+            <td>
+                <Link to={`/details/${_id}`}><button className="btn bg-pink-500 border-0 rancho">View Details</button></Link>
+            </td>
+        </tr>
     );
 };
 
--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -25,7 +25,7 @@ const AllToys = () => {
             setToys(jsonData);
         }
         fetchData()
-    }, [currentPage, productPerPage,setToys])
+    }, [currentPage, productPerPage, setToys])
 
     const handleSelectChange = (event) => {
         setProductPerPage(parseInt(event.target.value))
@@ -33,15 +33,35 @@ const AllToys = () => {
     }
 
     return (
-        <div className="my-24 flex flex-col justify-center">
+        <div className="my-12 flex flex-col justify-center">
             <input type="text" className="w-64 px-3 py-3 font-bold mb-14 border-2 rounded-xl mx-auto" placeholder="SEARCH..." onChange={e => setSearch(e.target.value)} />
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-sm md:max-w-3xl lg:max-w-7xl mx-auto">
-                {
-                    toys.filter(toy => toy.name.includes(search)).map(toy => <ShowToys
-                        key={toy._id}
-                        toy={toy}
-                    ></ShowToys>)
-                }
+            <div >
+                <div className="mx-14 mb-14">
+                    <div className="overflow-x-auto w-full">
+                        <table className="table w-full">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th>Picture</th>
+                                    <th>Name</th>
+                                    <th>Price</th>
+                                    <th>Quantity</th>
+                                    <th>Category</th>
+                                    <th>Seller</th>
+                                    <th>Details</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    toys.filter(toy => toy.name.includes(search)).map(toy => <ShowToys
+                                        key={toy._id}
+                                        toy={toy}
+                                    ></ShowToys>)
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
             {/* pagination */}
             <div className="text-center my-12">
